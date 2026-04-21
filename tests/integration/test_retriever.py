4de8872567235446d2d04b26ca08a9899d1dd02b
Brian McDowell <brianmcd08@gmail.com>
2026-04-21 10:20:40 -0400

    added test for retriever

---
+++ tests/integration/test_retriever.py
@@ -0,0 +1,16 @@
+from src.retrieval.retriever import CivRetriever
+
+
+def test_version_retrieved():
+    expected_version = "7.4"
+    query = "Which Great General gives you the Art of War?"
+    expected_answer = "Sun Tzu"
+    retriever = CivRetriever()
+
+    results = retriever.retrieve(query=query, version=expected_version)
+    assert results
+
+    for doc in results:
+        assert doc.metadata["bbg_version"] == expected_version
+
+    assert any(expected_answer in doc.page_content for doc in results)
